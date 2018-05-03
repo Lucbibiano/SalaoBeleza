725e87069c37ef49a7c2a1d7382abc43578801a0
VictorAlves3 <vtr.max@hotmail.com>
2018-05-03 17:01:45 -0300

    my local changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ServicosComponent } from './servicos/servicos.component';
-
-
 import { MaterializeModule } from 'angular2-materialize';
 
+import { ServicosComponent } from './servicos/servicos.component';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServicoService } from './shared/servico.service';
