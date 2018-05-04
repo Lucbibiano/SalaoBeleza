1ec5fab892089a46b5bc5c23e166bab558c83578
GustavoArsenio <gustavo.arsenio.sousa@Hotmail.com>
2018-05-04 12:01:16 -0300

    Correcao merge routing.module

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 0197d445a3173e90e6f349dace00dd31a87dab2a
 import { SobreComponent } from "./sobre/sobre.component";
 import { ServicosComponent } from "./servicos/servicos.component";
 import { HomeComponent } from "./home/home.component";
@@ -11,10 +7,7 @@ import { ProfissionaisComponent } from "./profissionais/profissionais.component"
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ServicoComponent } from "./servicos/servico/servico.component";
-<<<<<<< HEAD
-=======
 import { GerarImagensComponent } from "./gerar-imagens/gerar-imagens.component";
->>>>>>> 0197d445a3173e90e6f349dace00dd31a87dab2a
 
 
 const routing: Routes = [
@@ -29,21 +22,13 @@ const routing: Routes = [
        
      },
      { path:'servico/:id', component: ServicoComponent, 
-
-<<<<<<< HEAD
-            
-=======
->>>>>>> 0197d445a3173e90e6f349dace00dd31a87dab2a
     },
     { path:'home', component: HomeComponent,
     
     },
-<<<<<<< HEAD
-=======
     { path:'gerar', component: GerarImagensComponent,
     
-},
->>>>>>> 0197d445a3173e90e6f349dace00dd31a87dab2a
+
     { path:'agendamento', component: AgendarComponent},
 
     { path:'profissionais', component: ProfissionaisComponent}
